73d3790f80c4438837a70564c72f1389fc51f16f
Tseng, Ming Yu <qazwsx9006@hotmail.com>
2020-04-12 18:29:06 +0800

    fix get-mask-data.js timezone bug.

--- backend/get-mask-data.js
+++ backend/get-mask-data.js
@@ -27,73 +27,76 @@ async function fetchData() {
 
   const file = fs.createWriteStream("./maskdata.csv");
   console.log(`${new Date()}: start fetch maskData`);
-  https.get("https://data.nhi.gov.tw/resource/mask/maskdata.csv", response => {
-    var stream = response.pipe(file);
+  https.get(
+    "https://data.nhi.gov.tw/resource/mask/maskdata.csv",
+    (response) => {
+      var stream = response.pipe(file);
 
-    stream.on("finish", async function() {
-      console.log("download done");
-      console.log("start insert");
-      const r = fs.readFileSync("./maskdata.csv");
-      const datas = await neatCsv(r);
-      const dataChunks = _.chunk(datas, 20);
-      for (const dataChunk of dataChunks) {
-        const r = dataChunk.map(async store => {
-          const [
-            code,
-            name,
-            address,
-            _phone,
-            maskAdult,
-            maskChild,
-            updatedAt
-          ] = _.values(store);
-          let s = await Store.findById(code);
-          if (!s) s = new Store({ _id: code });
-          if (!s.name) s.name = name;
-          if (!s.address) s.address = address;
-          if (s.maskAdult > maskAdult) {
-            s.saleLog[currentWeekDay] = s.saleLog[currentWeekDay] || {};
-            s.saleLog[currentWeekDay][currentHour] =
-              s.saleLog[currentWeekDay][currentHour] || 0;
-            s.saleLog[currentWeekDay][currentHour] += s.maskAdult - maskAdult;
-            s.saleLog["near"] = {
-              time: new Date().getTime(),
-              number: s.maskAdult - maskAdult
-            };
-            if (maskAdult == 0)
-              s.saleLog[currentWeekDay]["soldOut"] = currentHour;
-            s.markModified("saleLog");
-          }
-          if (maskAdult > s.maskAdult) {
-            s.saleLog[currentWeekDay] = s.saleLog[currentWeekDay] || {};
-            s.saleLog[currentWeekDay]["add"] = {
-              currentHour,
-              number: maskAdult - s.maskAdult
-            };
+      stream.on("finish", async function () {
+        console.log("download done");
+        console.log("start insert");
+        const r = fs.readFileSync("./maskdata.csv");
+        const datas = await neatCsv(r);
+        const dataChunks = _.chunk(datas, 20);
+        for (const dataChunk of dataChunks) {
+          const r = dataChunk.map(async (store) => {
+            const [
+              code,
+              name,
+              address,
+              _phone,
+              maskAdult,
+              maskChild,
+              updatedAt,
+            ] = _.values(store);
+            let s = await Store.findById(code);
+            if (!s) s = new Store({ _id: code });
+            if (!s.name) s.name = name;
+            if (!s.address) s.address = address;
+            if (s.maskAdult > maskAdult) {
+              s.saleLog[currentWeekDay] = s.saleLog[currentWeekDay] || {};
+              s.saleLog[currentWeekDay][currentHour] =
+                s.saleLog[currentWeekDay][currentHour] || 0;
+              s.saleLog[currentWeekDay][currentHour] += s.maskAdult - maskAdult;
+              s.saleLog["near"] = {
+                time: new Date().getTime(),
+                number: s.maskAdult - maskAdult,
+              };
+              if (maskAdult == 0)
+                s.saleLog[currentWeekDay]["soldOut"] = currentHour;
+              s.markModified("saleLog");
+            }
+            if (maskAdult > s.maskAdult) {
+              s.saleLog[currentWeekDay] = s.saleLog[currentWeekDay] || {};
+              s.saleLog[currentWeekDay]["add"] = {
+                currentHour,
+                number: maskAdult - s.maskAdult,
+              };
 
-            s.addLog[currentWeekDay] = s.addLog[currentWeekDay] || {};
-            s.addLog[currentWeekDay][currentHour] =
-              s.addLog[currentWeekDay][currentHour] || 0;
-            s.addLog[currentWeekDay][currentHour] += maskAdult - s.maskAdult;
+              s.addLog[currentWeekDay] = s.addLog[currentWeekDay] || {};
+              s.addLog[currentWeekDay][currentHour] =
+                s.addLog[currentWeekDay][currentHour] || 0;
+              s.addLog[currentWeekDay][currentHour] += maskAdult - s.maskAdult;
 
-            s.markModified("saleLog");
-            s.markModified("addLog");
-          }
-          s.maskAdult = maskAdult;
-          s.maskChild = maskChild;
-          s.updatedAt = updatedAt;
-          if (!s.validateSync()) {
-            return s.save();
-          } else {
-            console.log(store);
-          }
-        });
-        await Promise.all(r);
-      }
-      console.log(`${new Date()} done`);
-      setTimeout(fetchData, 300000);
-    });
-  });
+              s.markModified("saleLog");
+              s.markModified("addLog");
+            }
+            s.maskAdult = maskAdult;
+            s.maskChild = maskChild;
+            s.updatedAt = new Date(`${updatedAt} +0800`);
+            if (!s.validateSync()) {
+              return s.save();
+            } else {
+              console.log(store);
+            }
+          });
+          await Promise.all(r);
+        }
+        console.log(`${new Date()} done`);
+        setTimeout(fetchData, 300000);
+      });
+    }
+  );
 }
 fetchData();
 
